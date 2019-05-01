e4efa97ed9b8ae32257941a11edcdce24ebef085
@faridiot <dev@designer.web.id>
2019-05-01 20:29:22 +0700

    dark

--- v6/js/app.js
+++ v6/js/app.js
@@ -128,3 +128,22 @@ $('#remove').click(function() {
 
 $('#val').text(localStorage.getItem("Pemustaka"));
 $('#val2').text(localStorage.getItem("Pemustaka"));
+
+
+
+
+function dark_toggle() {
+    var el1 = document.getElementById("dark-reader");
+    if(el1.disabled) {
+        el1.disabled = false;
+        localStorage.setItem("darkreader", "enabled");
+    } else {
+        el1.disabled = true;
+        localStorage.setItem("darkreader", "disabled");
+    }
+}
+ if (localStorage.getItem("darkreader") == "enabled") {
+     document.getElementById("dark-reader").disabled = false;
+ } else {
+     document.getElementById("dark-reader").disabled = true;
+ }
