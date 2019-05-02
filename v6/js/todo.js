3b56d31831c93a9974c6ec678d5cb1e7d1881240
@faridiot <dev@designer.web.id>
2019-05-02 19:06:30 +0700

    index

--- v6/js/todo.js
+++ v6/js/todo.js
@@ -29,8 +29,7 @@
     var storedValues = window.localStorage.myitems;
     if(!storedValues) {
       list.innerHTML = '<li>Mengunjungi perpustakaan</li>'+
-                       '<li>Mengerjakan tugas PAI</li>'+
-                       '<li>Mengembalikan buku Laskar Pelangi</li>'+
+                       '<li>Mengembalikan buku perpustakaan</li>'+
                        '<li>Meminjam novel</li>';
     }
     else {
