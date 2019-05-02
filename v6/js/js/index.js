abca50c9714ff50ec9a12faa1129777b1d55dfd0
@faridiot <dev@designer.web.id>
2019-05-02 10:57:02 +0700

    todo

---
+++ v6/js/js/index.js
@@ -0,0 +1,41 @@
+(function(){
+
+  var list = document.querySelector('#list'),
+      form = document.querySelector('form'),
+      item = document.querySelector('#item');
+
+  form.addEventListener('submit',function(e){
+    e.preventDefault();
+    list.innerHTML += '<li>' + item.value + '</li>';
+    store();
+    item.value = "";
+  },false)
+
+  list.addEventListener('click',function(e){
+    var t = e.target;
+    if(t.classList.contains('checked')){
+      t.parentNode.removeChild(t);
+    } else {
+      t.classList.add('checked');
+    }
+    store();
+  },false)
+
+  function store() {
+    window.localStorage.myitems = list.innerHTML;
+  }
+
+  function getValues() {
+    var storedValues = window.localStorage.myitems;
+    if(!storedValues) {
+      list.innerHTML = '<li>Mengunjungi perpustakaan</li>'+
+                       '<li>Mengerjakan tugas PAI</li>'+
+                       '<li>Mengembalikan buku Laskar Pelangi</li>'+
+                       '<li>Meminjam novel</li>';
+    }
+    else {
+      list.innerHTML = storedValues;
+    }
+  }
+  getValues();
+})();
